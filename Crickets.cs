9d5b09cc6866c5c92380d3b07b3214184ccc5526
Kubarev01 <113851142+Kubarev01@users.noreply.github.com>
2024-08-22 17:15:52 +0300

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //каждый кадр выводится crickets
+        Debug.Log("crickets");
+
+    }
+}
